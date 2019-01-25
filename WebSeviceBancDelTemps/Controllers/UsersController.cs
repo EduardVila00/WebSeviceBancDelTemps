627bfedbfb2bcd61ab92ae763f7738d66bac0c47
Eduard Vila <Eduard Vila@DESKTOP-FHEALRN>
2019-01-25 16:59:18 +0100

    Inici web service

---
+++ WebSeviceBancDelTemps/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebSeviceBancDelTemps.Controllers
+{
+    public class UsersController : ApiController
+    {
+        // GET: api/contactes
+        [Route("api/users")]
+        public HttpResponseMessage Get()
+        {
+            var contactes = ContactesRepository.GetAllContactes();
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, contactes);
+            return response;
+        }
+    }
+}
