c575b8a502ca9266b141b0a29035c4c5c132fca9
Eduard Vila <Eduard Vila@DESKTOP-FHEALRN>
2019-01-29 21:16:15 +0100

    Mes llistes users

--- WebSeviceBancDelTemps/Models/Repositoris/UsersRepository.cs
+++ WebSeviceBancDelTemps/Models/Repositoris/UsersRepository.cs
@@ -5,13 +5,24 @@ namespace WebSeviceBancDelTemps.Models.Repositoris
 {
     public class UsersRepository
     {
-        private  static BancDelTempsEntities db = new BancDelTempsEntities();
-        
+        private static BancDelTempsEntities db = new BancDelTempsEntities();
+
 
         public static List<User> GetAllUsers()
         {
-            //db.Configuration.ProxyCreationEnabled = false;
             var llistaUsers = db.Users.ToList();
+            return llistaUsers;
+        }
+
+        public static List<User> GetFiltreNameLastNameUsers(string filtre)
+        {
+            var llistaUsers = db.Users.Where(x => x.name.Contains(filtre) || x.last_name.Contains(filtre)).ToList();
+            return llistaUsers;
+        }
+
+        public static List<User> GetFiltreEmailUsers(string filtre)
+        {
+            var llistaUsers = db.Users.Where(x => x.email.Contains(filtre)).ToList();
             return llistaUsers;
         }
     }
