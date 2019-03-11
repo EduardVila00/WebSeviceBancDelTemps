f02ff0740a5206db5eb406843b0556f475c8399d
Eduard Vila <Eduard Vila@DESKTOP-FHEALRN>
2019-03-11 19:26:34 +0100

    Arreglat model

--- WebSeviceBancDelTemps/Models/Repositoris/Report.cs
+++ WebSeviceBancDelTemps/Models/Repositoris/Report.cs
@@ -14,19 +14,11 @@ namespace WebSeviceBancDelTemps.Models.Repositoris
     
     public partial class Report
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Report()
-        {
-            this.Pacts = new HashSet<Pact>();
-        }
-    
         public int Id_Report { get; set; }
         public string description { get; set; }
         public bool is_revised { get; set; }
         public int Post_Id_Post { get; set; }
     
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Pact> Pacts { get; set; }
         public virtual Post Post { get; set; }
     }
 }
